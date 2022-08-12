929be7ab3b07bcdfedb67822eb6762b8302f4b3b
Mario MG <mariomg11k@gmail.com>
2022-08-12 18:04:52 +0200

    Implement rotation - first steps

--- app/src/main/java/com/mariomg/circularrow/MainActivity.kt
+++ app/src/main/java/com/mariomg/circularrow/MainActivity.kt
@@ -3,19 +3,23 @@ package com.mariomg.circularrow
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.core.animateFloatAsState
-import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.unit.dp
 import com.mariomg.circularrow.ui.composables.CircularRow
+import com.mariomg.circularrow.ui.composables.PolarCoordinates
+import com.mariomg.circularrow.ui.composables.radToDeg
+import com.mariomg.circularrow.ui.composables.toPolarCoordinates
 import com.mariomg.circularrow.ui.theme.CircularRowTheme
 
 class MainActivity : ComponentActivity() {
@@ -28,23 +32,39 @@ class MainActivity : ComponentActivity() {
                         .fillMaxSize()
                         .background(MaterialTheme.colors.background),
                 ) {
-                    var numberOfClicks by remember { mutableStateOf(0) }
-                    val offset by animateFloatAsState(
-                        targetValue = numberOfClicks * 360f,
-                        animationSpec = tween(3000),
-                    )
+                    var center by remember { mutableStateOf(Offset(0f, 0f)) }
+                    var startPositionPolar by remember { mutableStateOf(PolarCoordinates(0f, 0f)) }
+                    var currentPositionPolar by remember { mutableStateOf(PolarCoordinates(0f, 0f)) }
+                    var offset by remember { mutableStateOf(0f) }
                     CircularRow(
-                        modifier = Modifier.weight(1f),
+                        modifier = Modifier
+                            .weight(1f)
+                            .onGloballyPositioned { coordinates ->
+                                val size = coordinates.size
+                                center = Offset(
+                                    x = size.width / 2f,
+                                    y = size.height / 2f,
+                                )
+                            }
+                            .pointerInput(Unit) {
+                                detectDragGestures { change, dragAmount ->
+                                    change.consume()
+                                    val startPositionFromCenter = change.previousPosition - center
+                                    val endPositionFromCenter =
+                                        startPositionFromCenter + dragAmount
+                                    startPositionPolar =
+                                        startPositionFromCenter.toPolarCoordinates()
+                                    currentPositionPolar =
+                                        endPositionFromCenter.toPolarCoordinates()
+                                    offset += (currentPositionPolar.angle - startPositionPolar.angle).radToDeg()
+                                }
+                            },
                         radius = 400.dp,
                         angularOffset = offset,
                     ) {
                         for (index in 1..5) {
                             Text("Prueba de CircularRow $index", modifier = Modifier.background(Color.Red))
                         }
-                    }
-
-                    Button(onClick = { numberOfClicks++ }) {
-                        Text("Rotate!")
                     }
                 }
             }
